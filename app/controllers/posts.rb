55f9b4514a0c1052e98ff6a4bd684612440bec3d
Bryan Heu <teamriab@gmail.com>
2013-03-17 16:51:23 -0700

    tags working and tweaks to css

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -5,22 +5,22 @@ end
 
 #on submit, we create the new Post object and save it to the db
 post "/posts" do
-  puts params
+  
   if @post = Post.create(params[:post])
     string = params[:tags] #  "apple, banana"
     array = string.gsub(" ", "").split(',') #["apple", "banana"]
     array.each do |name|
       #conditional to check if exists or not
       tag = Tag.where(name:name).first
-      if tag
+      if tag #exists, update the poststags table with the ids
         Poststag.create(post_id:@post.id, tag_id:tag.id)
-      else #tag is nil
+      else #tag is nil; create it first
         tag = Tag.create(name:name)
         Poststag.create(post_id:@post.id, tag_id:tag.id)
       end
     end
     redirect "posts/#{@post.id}" #if successful, redirect to post view page
-  else #otherwise go back to new post page (change to give error also)
+  else #otherwise go back to new post page
     erb :new
   end
 end
@@ -61,9 +61,38 @@ end
 #update the post
 put "/posts/:id" do
   @post = Post.find(params[:id])
+  #
   if @post.update_attributes(params[:post])
+    string = params[:tags] #  "apple, banana"
+    array = string.gsub(" ", "").split(',') #["apple", "banana"]
+    array.each do |name|
+      #conditional to check if exists or not
+      tag = Tag.where(name:name).first
+      if tag #exists, update the poststags table with the ids
+        Poststag.create(post_id:@post.id, tag_id:tag.id)
+      else #tag is nil; create it first
+        tag = Tag.create(name:name)
+        Poststag.create(post_id:@post.id, tag_id:tag.id)
+      end
+    end
+    
     redirect "/posts/#{@post.id}"
   else
+  
     erb :edit
   end
 end
+
+#get all posts with a tag
+
+get '/posts/tag/:name' do
+  # tag_id = Tag.find_by_name(params[:name])
+  # @post_ids = Poststag.where(:tag_id => tag_id)
+  # puts @post_ids
+
+  @tag = Tag.find_by_name(params[:name])
+  @posts = @tag.posts #returns all post objects that match the tag id passed
+  puts @posts
+
+  erb :tag_posts
+end  
