9b6c7b2d8029c45311a81b68121ac5601cf0941c
Bryan Heu <teamriab@gmail.com>
2013-03-17 18:37:17 -0700

    add'l css tweaks - LGTM

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   has_many :poststags
   has_many :tags, :through => :poststags
+
+  validates :body, :presence => true
+  validates :title, :presence => true
+
+  
 end
